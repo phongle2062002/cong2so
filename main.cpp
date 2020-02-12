6ceaf831ea9652648e9afb5612e335a8d4165a94
phongle2062002 <phongle2062002@gmail.com>
2020-02-12 19:23:39 +0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std; 
+#define PI 3.1416
+int main(int argc, char** argv) {
+	
+	float a,b,x;
+	cout<<"Nhap so a :";
+	cin >> a;
+	cout<<"Nhap so b :"; //Nh?p r t? bàn phím
+	cin >> b; 
+	x= a+b;
+	cout<<"Tong 2 so "<<x;
+	return 0;
+}
